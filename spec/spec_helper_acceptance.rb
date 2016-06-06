f5ed18b7478a30182a1b3cf1f3c695029197a1a7
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-06 14:09:16 +0200

    acceptance helper added

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,43 @@
+require 'beaker-rspec'
+
+# Load shared acceptance examples
+base_spec_dir = Pathname.new(File.join(File.dirname(__FILE__), 'acceptance'))
+Dir[base_spec_dir.join('shared/**/*.rb')].sort.each{ |f| require f }
+
+# Install Puppet
+unless ENV['RS_PROVISION'] == 'no'
+  # This will install the latest available package on el and deb based
+  # systems fail on windows and osx, and install via gem on other *nixes
+  foss_opts = { :default_action => 'gem_install' }
+
+  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
+
+  hosts.each do |host|
+    on host, "mkdir -p #{host['distmoduledir']}"
+  end
+end
+
+hosts.each do |host|
+  on host, 'mkdir -p /etc/facter/facts.d'
+  create_remote_file host, '/etc/facter/facts.d/packagecloud_facts.txt', "packagecloud_master_token=#{ENV['PACKAGECLOUD_MASTER_TOKEN']}", :protocol => 'rsync'
+end
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module and dependencies
+    puppet_module_install(:source => proj_root, :module_name => 'activemq')
+    hosts.each do |host|
+      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
+      shell('puppet module install puppetlabs-stdlib', { :acceptable_exit_codes => [0,1] })
+      shell('puppet module install puppetlabs-java', { :acceptable_exit_codes => [0,1] })
+      shell('puppet module install computology-packagecloud', { :acceptable_exit_codes => [0,1] })
+    end
+  end
+end
