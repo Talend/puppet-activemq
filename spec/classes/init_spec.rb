bfc25134379a6d6aa52261a8c0ad84dd64f2ce4a
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-06 14:43:23 +0200

    fixing tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -16,11 +16,10 @@ describe 'activemq' do
                     :ipaddress        => '192.168.0.1'
     }}
 
-    # Test if it compiles                                                                                                                                                                                
+    # Test if it compiles
     it { should compile }
-    it { should have_resource_count(6)}
 
-    # Test all default params are set                                                                                                                                                                    
+    # Test all default params are set
     it {
       should contain_class('activemq')
       should contain_class('activemq::params')
