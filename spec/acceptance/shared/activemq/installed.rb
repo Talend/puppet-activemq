2d7915fc880323c6c2ae8d55489c76931752a4d0
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-07 10:31:19 +0200

    activemq installing process specified

--- spec/acceptance/shared/activemq/installed.rb
+++ spec/acceptance/shared/activemq/installed.rb
@@ -11,6 +11,35 @@ shared_examples 'activemq::installed' do |parameters|
       type         => 'rpm',
       master_token => '#{fact('PACKAGECLOUD_MASTER_TOKEN')}'
     } ->
+    class { 'postgresql::globals':
+      version             => '9.3',
+      encoding            => 'UTF8',
+      locale              => 'en_NG',
+      manage_package_repo => true
+    } ->
+    class { 'postgresql::server':
+      listen_addresses   => '*'
+    } ->
+    postgresql::server::db { 'ams':
+      user     => 'ams',
+      password => 'ams'
+    } ->
+    file { '/var/tmp/amqsec.sql':
+      source => 'puppet:///modules/activemq/tmp/amqsec.sql'
+    } ->
+    file { '/var/tmp/ams.sql':
+      source => 'puppet:///modules/activemq/tmp/ams.sql'
+    } ->
+    exec { 'import ams db scheme':
+      environment => 'PGPASSWORD=ams',
+      command     => '/usr/bin/psql -U ams -h localhost -d ams -f /var/tmp/ams.sql && touch /tmp/ams.created',
+      creates     => '/tmp/ams.created'
+    } ->
+    exec { 'import amqsec db scheme':
+      environment => 'PGPASSWORD=ams',
+      command     => '/usr/bin/psql -U ams -h localhost -d ams -f /var/tmp/amqsec.sql && touch /tmp/amqsec.created',
+      creates     => '/tmp/amqsec.created'
+    } ->
     class { '::activemq':
       #{parameters.to_s}
     }
