a57d7266428bf6fc52e00af86cf4fba267e34fb0
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-07 10:33:34 +0200

    finishing test spec

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -2,8 +2,58 @@ require 'spec_helper_acceptance'
 
 describe 'activemq' do
 
-  context 'with default parameters' do
-    it_should_behave_like 'activemq::installed', ""
-    it_should_behave_like 'activemq::running'
+  it_should_behave_like 'activemq::installed', "
+    pg_db       => 'ams',
+    pg_username => 'ams',
+    pg_password => 'ams'
+  "
+  it_should_behave_like 'activemq::running'
+
+  context 'when accessed with proper credentials' do
+    describe command('/usr/bin/ruby /tmp/stomp-producer.rb tadmin tadmin ipaas.testing.notification.manager.queue test_data') do
+      its(:exit_status) { should eq 0 }
+    end
+
+    describe command('/usr/bin/ruby /tmp/stomp-consumer.rb tadmin tadmin ipaas.testing.notification.manager.queue') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should include 'test_data' }
+    end
   end
+
+  context 'when accessed with wrong username' do
+    describe command('/usr/bin/ruby /tmp/stomp-producer.rb nonexistentuser tadmin ipaas.testing.notification.manager.queue test_data') do
+      its(:exit_status) { should eq 1 }
+    end
+
+    describe file('/opt/activemq/data/activemq.log') do
+      its(:content) { should include 'User \'nonexistentuser\' not found or password is wrong.' }
+    end
+  end
+
+  context 'when accessed with wrong password' do
+    describe command('/usr/bin/ruby /tmp/stomp-producer.rb tadmin wrongpassword ipaas.testing.notification.manager.queue test_data') do
+      its(:exit_status) { should eq 1 }
+    end
+
+    describe file('/opt/activemq/data/activemq.log') do
+      its(:content) { should include 'User \'tadmin\' not found or password is wrong.' }
+    end
+  end
+
+  context 'when trying to work with a wrong queue' do
+    describe command('/usr/bin/ruby /tmp/stomp-producer.rb tadmin tadmin wrong.queue test_data') do
+      its(:exit_status) { should eq 0 }
+    end
+
+    describe command('/usr/bin/ruby /tmp/stomp-consumer.rb tadmin tadmin wrong.queue') do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should_not include 'test_data' }
+    end
+
+    describe file('/opt/activemq/data/activemq.log') do
+      its(:content) { should include 'User tadmin is not authorized to read from: queue://wrong.queue' }
+      its(:content) { should include 'User tadmin is not authorized to write to: queue://wrong.queue' }
+    end
+  end
+
 end
