cd005385544a5ce91cbbded3083991ef095f3d6b
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-07 10:31:06 +0200

    added activemq running spec

--- spec/acceptance/shared/activemq/running.rb
+++ spec/acceptance/shared/activemq/running.rb
@@ -1,4 +1,18 @@
 require 'spec_helper_acceptance'
 
 shared_examples 'activemq::running' do
+
+  describe service('activemq') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(8161) do
+    it { should be_listening }
+  end
+
+  describe port(5432) do
+    it { should be_listening }
+  end
+
 end
