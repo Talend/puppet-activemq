6d033743ab0e3e43424ed5e9a058ae95db1dcbed
Alexander Grigoriev <agrigoriev@talend.com>
2017-01-23 12:44:11 +0100

    updating the specs

--- spec/acceptance/activemq_spec.rb
+++ spec/acceptance/activemq_spec.rb
@@ -53,19 +53,11 @@ describe 'activemq' do
     describe command('/usr/bin/ruby /tmp/stomp-producer.rb nonexistentuser password ipaas.testing.notification.manager.queue test_data') do
       its(:exit_status) { should eq 1 }
     end
-
-    describe file('/opt/activemq/data/activemq.log') do
-      its(:content) { should include 'User \'nonexistentuser\' not found or password is wrong.' }
-    end
   end
 
   context 'when accessed with wrong password' do
     describe command('/usr/bin/ruby /tmp/stomp-producer.rb tadmin wrongpassword ipaas.testing.notification.manager.queue test_data') do
       its(:exit_status) { should eq 1 }
-    end
-
-    describe file('/opt/activemq/data/activemq.log') do
-      its(:content) { should include 'User \'tadmin\' not found or password is wrong.' }
     end
   end
 
