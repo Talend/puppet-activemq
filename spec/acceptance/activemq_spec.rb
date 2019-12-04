d459b04195645c5032e364d550c54ee46708802b
Erwan SEITE <eseite@talend.com>
2019-12-04 14:34:52 +0100

    Remove stomp connector

--- spec/acceptance/activemq_spec.rb
+++ spec/acceptance/activemq_spec.rb
@@ -32,7 +32,7 @@ describe 'activemq' do
 
     describe file('/opt/activemq/conf/activemq.xml') do
       its(:content) { should include '<transportConnector name="openwire" uri="tcp://0.0.0.0:61616?maximumConnections=5000&amp;wireFormat.maxFrameSize=500000"/>' }
-      its(:content) { should include '<transportConnector name="stomp" uri="stomp://0.0.0.0:61613?maximumConnections=5000&amp;wireFormat.maxFrameSize=500000"/>' }
+      its(:content) { should include '<transportConnector name="http" uri="http://0.0.0.0:8080?jetty.config=/opt/activemq/conf/jetty-server.xml&amp;wireFormat.maxFrameSize=500000"/>' }
       its(:content) { should include '<bean id="tipaasSecurityPlugin" class="org.talend.ipaas.rt.amq.security.TipaasSecurityPlugin"' }
       its(:content) { should include '<property name="activemqSecurityURL" value="http://localhost:9999/activemq-security-service/authenticate' }
       its(:content) { should include '<property name="refreshInterval" value="60000" />' }
